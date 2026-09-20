f19f657970c008dffcbed01b83ee1750f88449cf
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-21 06:13:01 +0800

    feat: add validated RewardTrace offsets

---
+++ include/RewardTraceOffsets.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <cstdint>
+
+namespace rewardtrace {
+inline constexpr const char *kExpectedUnityFrameworkUUID = "589A1CB1-1AC9-3E73-943A-B45C315C17FC";
+inline constexpr const char *kExpectedUnityFrameworkSHA256 = "3af327d13bd102a6ad5938c23272e601626657e6b463df455354a44aae988057";
+inline constexpr std::uint64_t kAdsPlayRewardMoveNext = 0x36184A0;
+inline constexpr std::uint64_t kAdsShowCompletionCallback = 0x3617464;
+inline constexpr std::uint64_t kMaxMediationShowRewardVideo = 0x8FDD29C;
+inline constexpr std::uint64_t kMaxReceivedRewardCallback = 0x8FDFF84;
+inline constexpr std::uint64_t kMaxHiddenCallback = 0x8FE03D4;
+inline constexpr std::uint64_t kMaxSdkShowRewardedAd = 0x86FCE58;
+inline constexpr std::uint64_t kCoopTicketsClick = 0x365D3D8;
+inline constexpr std::uint64_t kDailyFreePackClick = 0x36BDCFC;
+inline constexpr std::uint64_t kChuseokFreeClick = 0x3805F3C;
+inline constexpr std::uint64_t kCoopBoostClick = 0x3846518;
+inline constexpr std::uint64_t kCoopTicketsAdResult = 0x365D564;
+inline constexpr std::uint64_t kDailyFreePackAdResult = 0x36BDE38;
+inline constexpr std::uint64_t kChuseokAdResult = 0x38060BC;
+inline constexpr std::uint64_t kCoopBoostAdResult = 0x3848774;
+inline constexpr std::uint64_t kRpcCoopTicketsMoveNext = 0x379C0F4;
+inline constexpr std::uint64_t kRpcDailyFreePackMoveNext = 0x379C610;
+inline constexpr std::uint64_t kRpcChuseokMoveNext = 0x379BB0C;
+inline constexpr std::uint64_t kRpcCoopBoostMoveNext = 0x37A81B8;
+inline constexpr std::uint64_t kCoopTicketsSuccess = 0x365D560;
+inline constexpr std::uint64_t kDailyFreePackSuccess = 0x36BDE14;
+inline constexpr std::uint64_t kChuseokSuccess1 = 0x380609C;
+inline constexpr std::uint64_t kChuseokSuccess2 = 0x38060A8;
+inline constexpr std::uint64_t kCoopBoostSuccess = 0x3848844;
+inline constexpr std::uint64_t kHandleError = 0x3740A30;
+inline constexpr std::uint64_t kHandleServerResult = 0x3740E8C;
+inline constexpr std::uint64_t kHandleSuccessedResponse = 0x3743610;
+inline constexpr std::uint64_t kSetChuseokReceiveCount = 0x3629910;
+inline constexpr std::uint64_t kRefreshCoopTicketUI = 0x3844D98;
+} // namespace rewardtrace
